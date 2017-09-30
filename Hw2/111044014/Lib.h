3cdc45b0aa0b4acdfdc194d9907bdd35e74d2ac9
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-09-30 17:33:21 +0300

    Fixed File oparation

---
+++ Hw2/111044014/Lib.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include<iostream>
+#include <cstdlib>     /* srand, rand */
+#include <ctime>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+const int MAXSIZE = 20; // Game board max size 
+const int USER1PLAYERID = 1;
+const int USER2PLAYERID = 2;
+const int COMPUTERPLAYERID = 3;
+int SizeOfGame = 0;
+int GameMode = 0;
+const int TWO_PLAYER = 2;
+const int ONE_PLAYER_VERSUS_COMPUTER = 1;
+const char USER1 = 'X';
+const char USER2 = 'O';
+const char EMTHY = '.';
+
+char GameBoard[MAXSIZE][MAXSIZE];
---
+++ Hw2/111044014/main.h
@@ -0,0 +1,50 @@
+#include"Lib.h"
+
+struct NeigborEnemy
+{
+	int posX;
+	int posY;
+	string NeighborEnemyCounter;
+};
+
+
+void InputValidator();
+void PrintGameBoard();
+bool MoveInputCheck(const string& command);
+char TakeMove(const int& PlayerID);
+bool CommandSelector(const string& command);
+
+int MyStringCompare(const string& s1);
+int CheckCounter(const int& CurComp, const int& OtherComp, int count, const int& i, const int& j);
+
+string CheckLeftUpCross(const int& posX, const int& posY, const char&, const char& othercomparator, const int& size, const int& WinCounter, const bool& flag = false);
+string CheckLeftDownCross(const int& posX, const int& posY, const char&, const char& othercomparator, const int& size, const int& WinCounter, const bool& flag = false);
+string CheckRightUpCross(const int& posX, const int& posY, const char&, const char& othercomparator, const int& size, const int& WinCounter, const bool& flag = false);
+string CheckRightDownCross(const int& posX, const int& pos, const char&, const char& othercomparator, const int& size, const int& WinCounter, const bool& flag = false);
+
+
+string CheckLeft(const int& posX, const int& posY, const char&, const char& othercomparator, const int& size, const int& WinCounter, const bool& flag = false);
+string CheckRight(const int& posX, const int& posY, const char&, const char& othercomparato, const int& size, const int& WinCounterr, const bool& flag = false);
+string CheckUp(const int& posX, const int& posY, const char&, const char& othercomparator, const int& size, const int& WinCounter, const bool& flag = false);
+string CheckDown(const int& posX, const int& posY, const char&, const char& othercomparator, const int& size, const int& WinCounter, const bool& flag = false);
+bool IsPositionPlayable(const int& player_id, const char& pos);
+bool IsGameOverOneSide(const char& User, const char& other);
+int IsGameOver();
+bool AllMoveOperation(const int& PlayerID);
+
+
+
+
+void SaveFile(const string& filename);
+void LoadFile(const string& filename);
+
+
+bool AnyMoveMore();
+
+void Play();
+
+void MovePlayer(const int&,const char&);
+char MoveComputer();
+void FindComputerMove();
+
+void InitialBoard();

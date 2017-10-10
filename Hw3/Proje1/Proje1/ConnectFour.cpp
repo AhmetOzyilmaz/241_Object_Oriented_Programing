fe8caadc6f6df7b680a2848b33138ba4649c00b7
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-10-10 21:24:03 +0300

    Convertion homework 2

--- Hw3/Proje1/Proje1/ConnectFour.cpp
+++ Hw3/Proje1/Proje1/ConnectFour.cpp
@@ -1,14 +1,82 @@
 #include "ConnectFour.h"
 
 
+/*
+*	Desciription : This function saving gameboard status
+*	Input		   : conts string file name
+*	Return Value   : no return value
+*/
+void ConnectFour::SaveFile(const string& filename) {
+	ofstream myfile;
+	myfile.open(filename, std::ofstream::out | std::ofstream::app);
+	if (myfile.is_open()) {
+		//Oyun modu 1 se  user vs user 2 ise user vs computer 
+		//ikinci eleman oyunun size ý 
+		//TODO hamle kimde kaldýysa onun idsi CurrenPlayerID olarak atanýcak
 
-ConnectFour::ConnectFour()
-{
-
+		myfile << GetGameMode() << " " << GetGameCurrentSize() << " " << WhoIsWillPlay << endl;
+		for (int row = 0; row < GetGameCurrentSize(); ++row) {
+			for (int column = 0; column < GetGameCurrentSize(); ++column)
+				myfile << GameBoard[row][column];
+			if (row < GetGameCurrentSize() - 1)
+				myfile << "\n";
+		}
+		myfile.close();
+	}
+	else cerr << " <--->ILLEGAL<---> Unable to open file";
+	//cout << "Game Board Saved Correctly" << endl;
+}
+/*
+*	Desciription : This function loading saved gameboard from file
+*	Input		   : conts string file name
+*	Return Value   : no return value
+*/
+// TODO yanlýþ dosya yükleme,
+void ConnectFour::LoadFile(const string& filename) {
+	ifstream myReadFile;
+	myReadFile.open(filename);
+	string line = "";
+	int size = 0;
+	if (myReadFile.is_open()) {
+		int column = 0, row = 0;
+		getline(myReadFile, line);
+		if (!myReadFile.eof()) {
+			if (line.size() >= 5) {
+				GameMode = line[0] - '0';//ilk eleman oyun modu
+				SetGameCurrentSize(line[2] - '0');//2.deðiþken oyunun size ý
+				WhoIsWillPlay = line[4] - '0';//3.deðiþken oyunu þimdi kimin oynamasý gerektiði		
+			}
+			while (!myReadFile.eof()) {
+				getline(myReadFile, line);
+				for (int column = 0; column < line.size(); ++column)
+					GameBoard[row][column] = line[column];
+				++row;
+			}
+			SetGameCurrentSize(row);
+			cout << "Game Board Loaded Correctly\nNew Game Board is " << endl;
+			PrintGameBoard();
+		}
+		else
+			cerr << "<--->ILLEGAL<---> File is EMTHY  Please enter new file or keep going game " << endl;
+	}
+	myReadFile.close();
 }
 
+/*	Desciription : Printing screen current status of game board
+*	Input		 : no input parameter
+*	Return Value : no return value
+*/
 
-ConnectFour::~ConnectFour()
-{
-
+void ConnectFour::PrintGameBoard() {
+	char a = 'A', b = 'A';
+	while (a < b + GetGameCurrentSize()) {
+		cout << "  " << a << " ";
+		a++;
+	}
+	cout << endl;
+	for (auto i = 0; i < GetGameCurrentSize(); i++) {
+		for (auto j = 0; j < GetGameCurrentSize(); j++)
+			cout << "  " << GameBoard[i][j] << " ";
+		cout << "\n";
+	}
 }
--- Hw3/Proje1/Proje1/ConnectFour.h
+++ Hw3/Proje1/Proje1/ConnectFour.h
@@ -1,41 +1,58 @@
-#pragma once
+#include <iostream>
+#include<iostream>
+#include <cstdlib>     /* srand, rand */
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <vector>
 
-int ConnectFour:: CellCounter = 0;
+using namespace std;
+
 
 class ConnectFour
 {
 
 public:
-	ConnectFour();
-	~ConnectFour();
+	ConnectFour():ConnectFour(4) {	/*Can Be Emthy	*/ }
+	ConnectFour(int size):GameCurrentSize(size) {	/*Can Be Emthy	*/ }
+	//TODO at least 3 constructors
 
+	inline void SetGameCurrentSize(const int size) { GameCurrentSize = size; }
+	inline const int GetGameCurrentSize() { return GameCurrentSize; }
+
+	inline void SetGameMode(const int mode) { GameMode = mode; }
+	inline const int GetGameMode() { return GameMode; }
+
+	//File Operation
+	void SaveFile(const string& filename);
+	void LoadFile(const string& filename);
+	//End File Operation
+	void PrintGameBoard();	
 
 private:
-	static int CellCounter;
 	class Cell
 	{
 	public:
-		Cell() :PosColumn('A'), PosRow(0) {
-			//Can Be Emthy
-		}	
-		Cell(char PosC, int PosR) :PosColumn(PosC), PosRow(PosR) {
-			//Can Be Emthy
-
-		}
+		Cell() :PosColumn('A'), PosRow(0) {	/*Can Be Emthy	*/}	
+		Cell(char PosC, int PosR) :PosColumn(PosC), PosRow(PosR) {/*Can Be Emthy*/	}
 
 		inline void SetPosRow(const int row) { PosRow = row; }
 		inline void SetPosColumn(const char col) { PosColumn = col; }
-		inline int GetPosRow() { return PosRow ; }
-		inline char GetPosColumn() { return PosColumn; }
+		inline const int GetPosRow() { return PosRow ; }
+		inline const char GetPosColumn() { return PosColumn; }
 		inline void SetCellValue(const char col) { CellValue = col; }
-		inline char GetCellValue() { return CellValue; }
+		inline const char GetCellValue() { return CellValue; }
 
 	private:
 		char PosColumn = 'A';
 		int  PosRow    =  0;
-		char CellValue = '.'
+		char CellValue = '.';
 	};
 
+	static int CellCounter;
+	vector< vector<Cell> > gameCells;
+	int GameCurrentSize = 4; //default size
+	int GameMode = 1; //default game mode
 	
 };
 

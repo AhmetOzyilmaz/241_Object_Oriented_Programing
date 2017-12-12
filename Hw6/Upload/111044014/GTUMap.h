44c44b30641f4fa75238e8d9c395a757b4417798
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-12-12 11:56:35 +0300

    Homework 6 version 1
    when compile ubuntu it takes some errors

---
+++ Hw6/Upload/111044014/GTUMap.h
@@ -0,0 +1,35 @@
+#ifndef GTUMAP_H
+#define GTUMAP_H
+
+#include <iostream>
+#include <utility>
+
+#include "GTUSet.h"
+
+namespace GTU_NS {
+
+	template <class K, class V>
+	class GTUMap : public GTUSet<std::pair<K,V>>
+	{
+	public:
+		GTUMap(){}
+		~GTUMap(){}
+		V& operator[](const K& k) {
+			for (auto it = begin(); it != end(); it++)
+			{
+				if (k == (*it).first) {
+					return (*it).second;
+				}
+			}
+			V obj = V();
+			insert( std::pair<K,V>(k, obj) );
+			return (*this)[k];
+		}
+	private:
+
+
+	};
+
+}
+
+#endif

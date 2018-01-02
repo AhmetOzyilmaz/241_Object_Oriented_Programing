9fdb7f828f13d1a3ebf16d6e1f217cd2d78b0068
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2018-01-03 00:29:03 +0300

    Homework 8 Working version 1.0

--- Hw8/intelliji/src/Game.java
+++ Hw8/intelliji/src/Game.java
@@ -13,8 +13,31 @@ public class Game {
 
         game = new ConnectFour(height,width,mode);
     }
-    public void LoadGameBoard(){
+    //if game is ended return true else return false
+    public boolean Play(int move,int PlayerID){
 
+        if(game.getGameisEnded() == true){
+            System.out.println("GAME IS ENDED");
+            return true;
+
+        }
+        //if(PlayerID)
+        if(PlayerID == 3){
+            //Play computer
+            game.PlayMove();
+        }
+        else
+            game.PlayMove(PlayerID ,move);
+        game.PrintGameBoard();
+
+        int check = game.IsGameOver();
+        System.out.println("GAME CHECK" + check);
+        if (check == -1) {
+            game.setGameisEnded(true);
+            return true;
+        }
+
+        return false;
     }
 
 
